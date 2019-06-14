679b404a6a69b106d69d0d7dc9f771788c80ad75
willy850329 <48169892+willy850329@users.noreply.github.com>
2019-06-14 20:05:22 +0800

    Delete train.py

--- classifications/scripts/train.py
+++
@@ -1,2 +0,0 @@
-import audioTrainTest as aT
-aT.featureAndTrain(["../demo/train_COPD","../demo/train_normal"], 0.025, 0.025, aT.shortTermWindow, aT.shortTermStep, "randomforest", "classifier_rf", True)
